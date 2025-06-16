47eaa1a5afde338917e020ac90a660c2c0a34b6a
anhnlh <anh.lh.nguyen42@gmail.com>
2025-06-15 20:30:27 -0400

    nice little touch :)

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,8 @@ import Button from "../components/utils/LinkButton";
 export default function Index() {
   return (
     <div className="justify-center text-center">
-      <h1 className="mb-10 text-6xl">Welcome to the Rapid E-Store!</h1>
+      <h1 className="text-6xl">Welcome to the Rapid E-Store!</h1>
+      <h2 className="mt-3 mb-10 text-sm text-neutral-400">Made by anhnlh</h2>
       <Button text="Get Started" route="/home" />
     </div>
   );
